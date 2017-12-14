d1fe4d48c735e21c4d8f67ad2c59f491610c26ef
TOCANE TECHNOLOGIES <tocane.technologies@gmail.com>
2017-12-14 23:26:32 +0100

    upgrade to asp.net core 2.0

---
+++ TokenAuthWebApiCore.Server/SecurityContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TokenAuthWebApiCore.Server.Models;
+
+namespace TokenAuthWebApiCore.Server
+{
+    public class SecurityContext : IdentityDbContext<MyUser>
+    {
+        public SecurityContext(DbContextOptions<SecurityContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer(@"data source=(LocalDB)\MSSQLLocalDB;initial catalog=VS2017Db_TokenAuthWebApiCore.Server;persist security info=True;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework");
+            }
+        }
+    }
+}
